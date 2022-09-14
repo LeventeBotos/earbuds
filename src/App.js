37a9cfe65afc25bce66ee157facdd78961c2dfe1
Levente Botos <levinandi98@gmail.com>
2022-09-14 20:39:45 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./styles.css";
 import React from 'react'
+import Intro from './Intro'
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <p> EarBuds </p>
+      <Intro />
     </div>
   );
 }
