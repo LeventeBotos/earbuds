a65910153987344bd96f3c556ae1822ac609898c
Levente Botos <levinandi98@gmail.com>
2022-09-14 20:42:37 +0000

    Update Intro.jsx

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -5,8 +5,8 @@ export const Intro = () => {
         <div>
             <div class="flex vstack hstack@md">
                 <div className="AirPods"> AirPods </div>
-                <div className="AirPods"> AirPods </div>
-                <div className="AirPods"> AirPods </div>
+                <div className="AirPods3"> AirPods 3 </div>
+                <div className="AirPodsPro"> AirPods Pro </div>
             </div>    
         </div>    
     )
