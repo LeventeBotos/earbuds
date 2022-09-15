cbf71ac40ecbaa377922bfd82a82ec0bb795a8d1
Levente Botos <levinandi98@gmail.com>
2022-09-15 14:13:50 +0000

    Update 4 files

--- src/Features.jsx
+++ src/Features.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { BiCheck, BiX } from 'react-icons/bi'
+import './styles.css'
 
 export const Features = () => {
     return (
         <div>
             <h2> Features </h2>
-            
+            <table class="ifs" >
+                <tr class="bigtr" >
+                    <th> 
+                            <tr><th> </th></tr>
+                             <tr><th> ANC</th></tr>
+                    </th>
+                    <th>
+                             <tr><th> Sony WF-1000XM4</th></tr>
+                             <tr><th> <BiCheck /></th></tr>
+                    </th>
+                    <th>
+                             <tr><th> AirPods 3</th></tr>
+                             <tr><th> <BiX /></th></tr>
+                    </th>
+                    <th>
+                             <tr><th> AirPods Pro</th></tr>
+                             <tr><th> <BiCheck /></th></tr>
+                    </th>
+                    <th>
+                             <tr><th> AirPods Pro 2</th></tr>
+                             <tr><th> <BiCheck /></th></tr>
+                    </th>
+                </tr>
+            </table>
         </div>
     )
 }
