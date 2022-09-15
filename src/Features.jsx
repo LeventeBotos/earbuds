cc758132e94655930224b2718df89326e6f93bf2
Levente Botos <levinandi98@gmail.com>
2022-09-15 18:55:20 +0000

    ye

--- src/Features.jsx
+++ src/Features.jsx
@@ -11,22 +11,27 @@ export const Features = () => {
                     <th> 
                             <tr><th> </th></tr>
                              <tr><th> ANC</th></tr>
+                             <tr><th> Battery life</th></tr>
                     </th>
                     <th>
                              <tr><th> Sony WF-1000XM4</th></tr>
                              <tr><th> <BiCheck /></th></tr>
+                               <tr><th> 8</th></tr>
                     </th>
                     <th>
                              <tr><th> AirPods 3</th></tr>
                              <tr><th> <BiX /></th></tr>
+                               <tr><th> 7</th></tr>
                     </th>
                     <th>
                              <tr><th> AirPods Pro</th></tr>
                              <tr><th> <BiCheck /></th></tr>
+                               <tr><th> 6</th></tr>
                     </th>
                     <th>
                              <tr><th> AirPods Pro 2</th></tr>
                              <tr><th> <BiCheck /></th></tr>
+                               <tr><th> 8</th></tr>
                     </th>
                 </tr>
             </table>
