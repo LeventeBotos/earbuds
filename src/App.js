181db8cdde942058fcb6a94c968841dc6d15d2e8
Levente Botos <levinandi98@gmail.com>
2022-09-15 12:04:50 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import React from 'react'
 import Intro from './Intro'
+import Features from './Features'
 
 export default function App() {
   return (
     <div className="App">
       <h1 class="grad"> EarBuds </h1>
       <Intro />
+      <Features />
     </div>
   );
 }
